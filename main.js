06c206ef7c43f7f036c9a67331db5cb499e55730
Vitor Estevam <vitorestevam02@gmail.com>
2021-04-04 20:49:07 -0300

    about

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ function initApp(data) {
         data: {
             steps: data,
             open: false,
-            open2:true,
+            open2:false,
             refs:[],
             step: data[0],
             swiper2: Object,
